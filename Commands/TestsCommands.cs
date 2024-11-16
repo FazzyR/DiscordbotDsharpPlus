0f3d11f8a357961618be992494a3b3d43eaf18d5
Louay Djabali <louay2005123@gmail.com>
2024-11-16 17:17:23 +0100

    Add project files.

---
+++ Commands/TestsCommands.cs
@@ -0,0 +1,112 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplication.Other;
+
+namespace TestApplication.Commands
+{
+    public class TestsCommands:BaseCommandModule    
+    {
+        
+
+        [Command("add")]
+        public async Task add(CommandContext ctx,int num1,int num2)
+        {
+            int result = num1 + num2;
+            await ctx.Channel.SendMessageAsync(result.ToString());
+        }
+
+        [Command("qst")]
+        public async Task Qst(CommandContext ctx,[RemainingText]string qst=null)
+        {
+            // Check if the user didn't provide a question
+            if (string.IsNullOrWhiteSpace(qst))
+            {
+                await ctx.Channel.SendMessageAsync("AKTB QUESTION YA MONGOL");
+                return; // Exit the method early
+            }
+
+            // Create a random number generator
+            Random random = new Random();
+
+            // Generate a random number: 0 or 1
+            int choice = random.Next(0, 2); // 0 for "No", 1 for "Yes"
+
+            // Determine the response
+            string response = choice == 1 ? "Yes" : "No";
+
+            // Send the response
+            await ctx.Channel.SendMessageAsync(response);
+        }
+
+
+        [Command("cardgame")]
+        public async Task Qst(CommandContext ctx)
+        {
+            var userCard = new CardSystem();
+
+            var userCardEmbed = new DiscordEmbedBuilder 
+            {
+                Title=$"{ctx.User.Username.ToUpper()} Card is {userCard.SelectedCard} ",
+                Color=DiscordColor.Lilac,
+
+            
+            };
+            
+            await ctx.Channel.SendMessageAsync(embed: userCardEmbed);
+
+            var botCard = new CardSystem();
+            var botCardEmbed = new DiscordEmbedBuilder
+            {
+                Title = $"The bot drew {botCard.SelectedCard} ",
+                Color = DiscordColor.Red,
+
+
+            };
+            await ctx.Channel.SendMessageAsync(embed: botCardEmbed);
+
+            if(userCard.SelectedNumber > botCard.SelectedNumber)
+            {
+                var winMessage = new DiscordEmbedBuilder
+                {
+                    Title = $" Congratulations {ctx.User.Username.ToUpper()} WIN!!! ",
+                    Color = DiscordColor.Green,
+
+
+                };
+                await ctx.Channel.SendMessageAsync(embed: winMessage);
+
+            }
+            else if (userCard.SelectedNumber == botCard.SelectedNumber) 
+            {
+                var drawMessage = new DiscordEmbedBuilder
+                {
+                    Title = $"DRAAAAAAAAAAW !!!!!!!!! ",
+                    Color = DiscordColor.Gray,
+
+
+                };
+                await ctx.Channel.SendMessageAsync(embed: drawMessage);
+            }
+            else
+            {
+                var loseMessage = new DiscordEmbedBuilder
+                {
+                    Title = $" {ctx.User.Username.ToUpper()} Have lost the game HAHAHAHAHAHHA!!! ",
+                    Color = DiscordColor.Red,
+
+
+                };
+                await ctx.Channel.SendMessageAsync(embed: loseMessage);
+
+            }
+
+        }
+        public TestsCommands() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplication.Commands;
+using TestApplication.config;
+
+namespace TestApplication
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents=DiscordIntents.All,
+                Token=jsonReader.token,
+                TokenType=TokenType.Bot,
+                AutoReconnect=true,
+            };
+
+            Client= new DiscordClient(discordConfig);
+
+
+            Client.Ready += Client_Ready;
+            Client.MessageCreated += Client_MessageCreated;
+
+
+            var CommandsConfig =new CommandsNextConfiguration() 
+            {
+            
+                StringPrefixes =new string[] {jsonReader.prefix},
+                EnableMentionPrefix =true,
+                EnableDms=true, 
+                EnableDefaultHelp=false,
+
+            };
+
+            Commands =Client.UseCommandsNext(CommandsConfig);
+
+            Commands.RegisterCommands<TestsCommands>();
+            await Client.ConnectAsync();
+            
+
+            await Task.Delay(-1);   
+
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+           return Task.CompletedTask;
+        }
+        private static async Task Client_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            // Ignore messages from bots
+            if (args.Author.IsBot) return;
+
+            // Check if the message is the name of the command
+            if (args.Message.Content.Equals("Fazzy", StringComparison.OrdinalIgnoreCase) || args.Message.Content.Equals("louay", StringComparison.OrdinalIgnoreCase)|| args.Message.Content.Equals("loay", StringComparison.OrdinalIgnoreCase))
+            {
+                // Respond to the command
+                await args.Message.RespondAsync($"Wach {args.Author.Username}");
+            }
+            // Check if the message is the name of the command
+            if (args.Message.Content.Equals("Fazzy Z Goat", StringComparison.OrdinalIgnoreCase))
+            {
+                // Respond to the command
+                await args.Message.RespondAsync("🐐");
+            }
+        }
+    }
+}
